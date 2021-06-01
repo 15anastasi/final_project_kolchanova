fadf8b71398aae762339eace0e7d5d20be823c1c
15anastasi <85166671+15anastasi@users.noreply.github.com>
2021-06-01 19:57:01 +0500

    Update kolchanova.R

--- kolchanova.R
+++ kolchanova.R
@@ -13,3 +13,25 @@ library(rattle)
 library(GoodmanKruskal)
 library(arm)
 library(randomForest)
+
+hotel_data <- read.csv('../input/hotel-booking-demand/hotel_bookings.csv')
+
+cols(
+  .default = col_double(),
+  hotel = col_character(),
+  arrival_date_month = col_character(),
+  meal = col_character(),
+  country = col_character(),
+  market_segment = col_character(),
+  distribution_channel = col_character(),
+  reserved_room_type = col_character(),
+  assigned_room_type = col_character(),
+  deposit_type = col_character(),
+  agent = col_character(),
+  company = col_character(),
+  customer_type = col_character(),
+  reservation_status = col_character(),
+  reservation_status_date = col_date(format = "")
+)
+hotel_data[sapply(hotel_data, is.character)] <-
+  lapply(hotel_data[sapply(hotel_data, is.character)], as.factor)
